347f94d0749121bfbfe76a412d32829cc4ba8e25
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-02 19:49:08 +0300

    setting up api

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    article_heading = models.CharField(max_length=250)
+    article_body = models.TextField()
--- articlevue/settings.py
+++ articlevue/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles,'
+    'articles.apps.ArticlesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
